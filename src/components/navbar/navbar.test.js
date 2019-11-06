50d228e1375e78d589ab73242228aa33a0fa2a19
nphiukhao <nphiukhao@gmail.com>
2019-11-05 18:05:56 -0800

    navbar snapshot updated

--- src/components/navbar/navbar.test.js
+++ src/components/navbar/navbar.test.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Navbar from './navbar'
 import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
+import PropTypes from 'prop-types'
+
+class ContextProvider extends Component {
+
+    static childContextTypes = {
+        myContext: PropTypes.object
+    }
+
+    getChildContext = () => ({
+        myContext: {
+            isLoggedIn: true
+        }
+    })
+    render(){
+        return this.props.children
+    }
+}
 
 describe(`nav bar component`, () => {
     it('nav bar snapshot test', () => {
-        const tree = shallow(<Navbar/>)
+        const tree = shallow(<ContextProvider><Navbar /></ContextProvider>)
         expect(toJson(tree)).toMatchSnapshot()
     })
 })
--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000',
+    API_ENDPOINT: 'https://mighty-refuge-14158.herokuapp.com',
     TOKEN_KEY: process.env.REACT_APP_API_KEY
 }
-// 'https://mighty-refuge-14158.herokuapp.com'
