d7e4566101d7afb4a25f351ce15d7e2dd94fe028
nphiukhao <nphiukhao@gmail.com>
2020-03-15 22:12:18 -0700

    instructions get working

--- src/components/BrowseRecipe/BrowseRecipe.js
+++ src/components/BrowseRecipe/BrowseRecipe.js
@@ -1,36 +1,62 @@
 import React, { Component } from "react";
-import SpoonContext from '../../context/SpoonContext'
+import SpoonContext from "../../context/SpoonContext";
+import "./BrowseRecipe.css";
 
 export default class BrowseRecipe extends Component {
-
   static contextType = SpoonContext;
 
-
-
   componentDidMount() {
-    let id = this.props.match.params.id
-    fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=90983f8a705146c39a2acfcb0c8b7f28`)
-      .then(res => !res.ok ? res.json().then(e => Promise.reject(e)) : res.json())
-      .then(result => this.context.buildIngred(result.extendedIngredients))
+    let id = this.props.match.params.id;
+    fetch(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=90983f8a705146c39a2acfcb0c8b7f28`
+    )
+      .then(res =>
+        !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+      )
+      .then(result => {
+        this.context.buildIngred(result.extendedIngredients);
+        this.context.updateInstructions(result.analyzedInstructions[0].steps);
+      });
   }
   renderIngredients = () => {
-    console.log(this.context.ingredients)
-    return <ul className='ingredients'>
-      {this.context.ingredients.map((ingred, index) => {
-        console.log('index: ', index)
-        return (
-        <li key={index}>{ingred.amount} {ingred.unit} {ingred.name}</li>
-        )
-      })}
-    </ul>
-  }
+    return (
+      <ul className="ingredients">
+        {this.context.ingredients.map((ingred, index) => {
+          return (
+            <li key={index}>
+              {ingred.amount} {ingred.unit} {ingred.name}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+
+  renderInstructions = () => {
+    console.log("rendering instructions");
+    return (
+      <ul className="instructions">
+        {this.context.instructions.map((instruction, index) => {
+          return (
+            <li key={index}>
+              {instruction.number}. {instruction.step}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
 
   render() {
-    console.log('inside single recipe page')
     return (
       <div>
         <h2> Single Recipe Page</h2>
-        {this.context.ingredients ? this.renderIngredients() : null }
+        <div className="ingredients">
+          {this.context.ingredients ? this.renderIngredients() : null}
+        </div>
+        <div className="instructions">
+          {this.context.instructions ? this.renderInstructions() : null}
+        </div>
       </div>
     );
   }
--- src/context/SpoonContext.js
+++ src/context/SpoonContext.js
@@ -15,6 +15,7 @@ const SpoonContext = React.createContext({
   setResults: () => {},
   clearData: () => {},
   updateIngredients: () => {},
+  updateInstructions: () => {},
   buildIngred: () => {},
 });
 
@@ -43,18 +44,16 @@ export class SpoonProvider extends Component {
     })
   }
   updateIngredients = result => {
-    // console.log("setting the state of ingreds");
+    console.log("setting the state of ingreds", result);
     this.setState({
       ingredients: result
     });
   };
 
   buildIngred = ingredResult => {
-    console.log("inside bildIngred");
     let ingredientsArr = [];
     ingredResult.forEach(ingred => {
       ingredientsArr.push({
-        // id: ingred.id,
         name: ingred.name,
         amount: ingred.measures.us.amount,
         unit: ingred.measures.us.unitShort
@@ -63,14 +62,24 @@ export class SpoonProvider extends Component {
     this.updateIngredients(ingredientsArr);
   };
 
+  updateInstructions = instructions => {
+    console.log('updating instructions', instructions);
+    this.setState({
+      instructions:instructions
+    })
+    console.log('this is instructions in state',this.state.instructions)
+  }
+
 
   render() {
     const value = {
       result: this.state.result,
       ingredients: this.state.ingredients,
+      instructions: this.state.instructions,
       setResults: this.setResults,
       clearData: this.clearData,
       updateIngredients: this.updateIngredients,
+      updateInstructions: this.updateInstructions,
       buildIngred: this.buildIngred
     };
     return (
